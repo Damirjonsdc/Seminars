4665e2609116743b4e4c2bc56ef9f8c7097d6714
Damir <ahmedjanovdamir65@gmail.com>
2023-03-28 21:05:02 +0500

    added info about 1 task

---
+++ Homework Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CompareNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"{num1} Больше,чем {num2}");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine($"{num1} Меньше,чем {num2}");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} и {num2} равны");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
