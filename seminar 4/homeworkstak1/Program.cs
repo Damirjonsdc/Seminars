2252247e32d7d6263373edbdd4440052d45b3b16
Damir <ahmedjanovdamir65@gmail.com>
2023-04-02 20:05:23 +0500

    homework 4sem

--- seminar 4/homeworkstak1/Program.cs
+++ seminar 4/homeworkstak1/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число A: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите степень B: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int result = 1;
+
+        for (int i = 1; i <= b; i++)
+        {
+            result *= a;
+        }
+
+        Console.WriteLine("{0} в степени {1} равно: {2}", a, b, result);
+
+        Console.ReadKey();
+    }
+}
---
+++ seminar 4/homeworktask2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        string str = Console.ReadLine();
+
+        int sum = 0;
+        foreach (char ch in str)
+            sum += ch - '0';
+
+        Console.WriteLine($"Сумма цифр в числе {str} равна {sum}");
+    }
+}
