9f6754055582422237f4b7293939a89ea8b1a4fb
Damir <ahmedjanovdamir65@gmail.com>
2023-04-21 21:26:57 +0500

    added info about changes

--- Seminar8/homewprktask5/Program.cs
+++ Seminar8/homewprktask5/Program.cs
@@ -1,51 +1,57 @@
 ﻿ /*
  Напишите программу, которая заполнит спирально массив 4 на 4.
  */
- 
-using System;
+int[,] arr = new int[4, 4];
 
-class Program {
-    static void Main(string[] args) {
-        int[,] array = new int[4,4];
-        int value = 1;
-        int rows = 4, cols = 4;
-        int startRow = 0, startCol = 0;
-        while (rows > 0 && cols > 0) {
-            // заполнение верхней строки
-            for (int i = 0; i < cols; i++) {
-                array[startRow, startCol + i] = value++;
-            }
-            startRow++;
+int counter = 1;
+int rowStart = 0;
+int rowEnd = arr.GetLength(0) - 1;
+int colStart = 0;
+int colEnd = arr.GetLength(1) - 1;
 
-            // заполнение правого столбца
-            for (int i = 0; i < rows - 1; i++) {
-                array[startRow + i, startCol + cols - 1] = value++;
-            }
-            cols--;
-
-            // заполнение нижней строки
-            if (rows > 1) {
-                for (int i = cols - 1; i >= 0; i--) {
-                    array[startRow + rows - 1, startCol + i] = value++;
-                }
-                rows--;
-            }
-
-            // заполнение левого столбца
-            if (cols > 1) {
-                for (int i = rows - 1; i >= 1; i--) {
-                    array[startRow + i, startCol] = value++;
-                }
-                startCol++;
-            }
-        }
-
-        // вывод массива
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                Console.Write(array[i, j] + "\t");
-            }
-            Console.WriteLine();
-        }
+while (rowStart <= rowEnd && colStart <= colEnd)
+{
+    // Fill the top row
+    for (int i = colStart; i <= colEnd; i++)
+    {
+        arr[rowStart, i] = counter++;
     }
+    rowStart++;
+
+    // Fill the right column
+    for (int i = rowStart; i <= rowEnd; i++)
+    {
+        arr[i, colEnd] = counter++;
+    }
+    colEnd--;
+
+    // Fill the bottom row
+    if (rowStart <= rowEnd)
+    {
+        for (int i = colEnd; i >= colStart; i--)
+        {
+            arr[rowEnd, i] = counter++;
+        }
+        rowEnd--;
+    }
+
+    // Fill the left column
+    if (colStart <= colEnd)
+    {
+        for (int i = rowEnd; i >= rowStart; i--)
+        {
+            arr[i, colStart] = counter++;
+        }
+        colStart++;
+    }
+}
+
+// Print the resulting array
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write(arr[i, j] + "\t");
+    }
+    Console.WriteLine();
 }
